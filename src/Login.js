a753a12b8907dab79d1c965d2d6e41dffb71d757
Michael <68960336+Mikewon98@users.noreply.github.com>
2023-01-05 08:16:55 -0800

    first commit

---
+++ src/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <div className='login'>
+      <img
+        src='/vinnytsia-ukraine-may-spotify-logo-dark-mode-cool-music-service-offers-legal-streaming-music-spotify-logo-dark-mode-218033923.jpg'
+        alt='spotify'
+      />
+      <a>Login with spotify</a>
+    </div>
+  );
+}
+
+export default Login;
